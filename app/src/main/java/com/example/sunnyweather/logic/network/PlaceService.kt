957a8245d22e0d98b0c3b8ae52437091544d313e
xiao <jiangx.cn@gmail.com>
2021-11-14 21:47:38 +0100

    final

--- app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -1,12 +1,13 @@
 package com.example.sunnyweather.logic.network
 
+import com.example.sunnyweather.SunnyWeatherApplication
 import com.example.sunnyweather.logic.model.PlaceResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PlaceService {
-    @GET("v2/place?token={SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
     //searchPlaces()方法的返回值被声明成了Call<PlaceResponse>，这样Retrofit就会将服务器返回的JSON数据自动解析成PlaceResponse对象了
 }
